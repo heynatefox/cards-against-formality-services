0516914d595e7bfa767e830169cce1ef506f72b4
Jordan Pawlett <jordanpawlett@googlemail.com>
2020-11-02 19:56:02 +0000

    Fix appearence of duplicates cards. Do not error if the user submits their cards again. Add function to calculate if the game has enough cards left to continue

--- services/games-service/src/turn.ts
+++ services/games-service/src/turn.ts
@@ -35,10 +35,16 @@ export interface TurnDataWithState extends TurnData {
 
 export default class TurnHandler {
 
-  constructor(protected broker: ServiceBroker, protected logger: LoggerInstance) {
+  constructor(protected broker: ServiceBroker, protected logger: LoggerInstance) { }
 
-  }
-
+  /**
+   * Given a list of deck ids. Return all white and black cards associated to the decks.
+   *
+   * @protected
+   * @param {string[]} deckIds
+   * @returns {Promise<{ whiteCards: string[]; blackCards: string[] }>}
+   * @memberof TurnHandler
+   */
   protected fetchCards(deckIds: string[]): Promise<{ whiteCards: string[]; blackCards: string[] }> {
     const _whiteCards = [];
     const _blackCards = [];
@@ -53,10 +59,27 @@ export default class TurnHandler {
       });
   }
 
+  /**
+   * Return a random number between 0 and the upperlimit.
+   *
+   * @private
+   * @param {number} upperLimit
+   * @returns {number}
+   * @memberof TurnHandler
+   */
   private getRandomIndex(upperLimit: number): number {
     return Math.round(Math.random() * upperLimit);
   }
 
+  /**
+   * Given a list of players choose the next czar. Mutates player by reference.
+   *
+   * @private
+   * @param {TurnDataWithState[]} turns
+   * @param {{ [id: string]: GamePlayer }} players
+   * @returns {string}
+   * @memberof TurnHandler
+   */
   private pickCzar(turns: TurnDataWithState[], players: { [id: string]: GamePlayer }): string {
     // get the previous rounds czar.
     const turnsLength = turns.length;
@@ -66,7 +89,7 @@ export default class TurnHandler {
       prevCzar = prevTurn.czar;
     }
 
-    // pick czar and emit update.
+    // pick czar
     const playersArr = Object.values(players);
     let selectedPlayer;
     if (!prevCzar) {
@@ -87,28 +110,61 @@ export default class TurnHandler {
     return selectedPlayer._id;
   }
 
+  /**
+   * Given a set of black cards, Return a random black card.
+   *
+   * @private
+   * @param {string[]} blackCards
+   * @returns {Promise<Card>}
+   * @memberof TurnHandler
+   */
   private pickBlackCard(blackCards: string[]): Promise<Card> {
     const index = this.getRandomIndex(blackCards.length - 1);
+    const id = blackCards[index];
     // Remove the card so it cannot be chosen again.
     blackCards.splice(index, 1);
-    const id = blackCards[index];
     return this.broker.call('cards.get', { id });
   }
 
+  /**
+   * Givn a set of white cards, return a random white card.
+   *
+   * @private
+   * @param {string[]} whiteCards
+   * @returns {string}
+   * @memberof TurnHandler
+   */
   private pickWhiteCard(whiteCards: string[]): string {
     const index = this.getRandomIndex(whiteCards.length - 1);
+    const id = whiteCards[index];
     // Remove the card so it cannot be chosen again.
     whiteCards.splice(index, 1);
-    return whiteCards[index];
+    return id;
   }
 
+  /**
+   * Given a Player, fetch their cards and emit all their cards to them.
+   *
+   * @private
+   * @param {GamePlayer} player
+   * @returns
+   * @memberof TurnHandler
+   */
   private emitCardsToPlayer(player: GamePlayer) {
     // Get all the cards and deal them to the player.
     return this.broker.call('cards.get', { id: player.cards })
       .then(cards => this.broker.emit('games.deal', { clientId: player._id, cards }));
   }
 
-  // Given a player, deal all the white cards to it.
+  /**
+   * Deal white cards to a Player until they have 10 cards.
+   *
+   * @protected
+   * @param {GamePlayer} player
+   * @param {string[]} whiteCards
+   * @returns {Promise<string[]>}
+   * @memberof TurnHandler
+   */
   protected async dealWhiteCards(player: GamePlayer, whiteCards: string[]): Promise<string[]> {
     const cardsNeeded = 10 - player.cards.length;
     if (!cardsNeeded) {
@@ -124,6 +180,15 @@ export default class TurnHandler {
     return whiteCards;
   }
 
+  /**
+   * Ensure each Player has the correct amount of cards for the turn.
+   *
+   * @private
+   * @param {{ [id: string]: GamePlayer }} players
+   * @param {string[]} whiteCards
+   * @returns
+   * @memberof TurnHandler
+   */
   private async ensurePlayersHaveCards(players: { [id: string]: GamePlayer }, whiteCards: string[]) {
     for (const player of Object.values(players)) {
       whiteCards = await this.dealWhiteCards(player, whiteCards);
@@ -132,8 +197,36 @@ export default class TurnHandler {
     return whiteCards;
   }
 
+  /**
+   * Calculate whether there are enough cards left to proceed based upon the number of players.
+   *
+   * @param {GamePlayer[]} players
+   * @param {string[]} whiteCards
+   * @param {string[]} blackCards
+   * @returns {boolean}
+   * @memberof TurnHandler
+   */
+  public hasEnoughCards(players: GamePlayer[], whiteCards: string[], blackCards: string[]): boolean {
+    // check if there are enough cards left to play the turn.
+    const whiteCardsRequired = Object.values(players).reduce((totalRequired, player) => {
+      const cardsRequired = 10 - player.cards.length;
+      return totalRequired + cardsRequired;
+    }, 0);
+
+    return whiteCardsRequired < whiteCards.length && !!blackCards.length;
+  }
+
+  /**
+   * Given the game state, prepare the game for the start of the turn.
+   *
+   * @protected
+   * @param {GameInterface} game
+   * @returns {Promise<TurnDataWithState>}
+   * @memberof TurnHandler
+   */
   protected async startTurn(game: GameInterface): Promise<TurnDataWithState> {
     const { turnData, players, room, turns, whiteCards, blackCards } = game;
+
     // mutate by reference. ensure we reset the czar.
     Object.values(players).forEach(player => player.isCzar = false);
 
@@ -158,6 +251,16 @@ export default class TurnHandler {
     };
   }
 
+  /**
+   * Set the Players selected cards for the round.
+   *
+   * @protected
+   * @param {GameInterface} game
+   * @param {string} clientId
+   * @param {string[]} cards
+   * @returns {Promise<GameInterface>}
+   * @memberof TurnHandler
+   */
   protected async submitCards(game: GameInterface, clientId: string, cards: string[]): Promise<GameInterface> {
     const { selectedCards, turnData, players } = game;
 
@@ -165,7 +268,7 @@ export default class TurnHandler {
       throw new Error('The czar is not allowed to play the round.');
     }
     if (clientId in selectedCards) {
-      throw new Error('You have already submitted your cards.');
+      return this.broker.call('games.get', { id: game._id, populate: ['room'] });
     }
 
     if (turnData.blackCard.pick !== cards.length) {
@@ -185,6 +288,15 @@ export default class TurnHandler {
     return this.broker.call('games.get', { id: game._id, populate: ['room'] });
   }
 
+  /**
+   * Fetch the winning cards. Based on the winner.
+   *
+   * @protected
+   * @param {{ [id: string]: string[] }} selectedCards
+   * @param {string} winner
+   * @returns {Promise<Card[]>}
+   * @memberof TurnHandler
+   */
   protected selectWinner(selectedCards: { [id: string]: string[] }, winner: string): Promise<Card[]> {
     const winningCards = selectedCards[winner];
     return this.broker.call('cards.get', { id: winningCards })
@@ -194,6 +306,14 @@ export default class TurnHandler {
       });
   }
 
+  /**
+   * Transform the string ids into Cards.
+   *
+   * @protected
+   * @param {{ [id: string]: string[] }} selectedCards
+   * @returns
+   * @memberof TurnHandler
+   */
   protected async populatedSelectedCards(selectedCards: { [id: string]: string[] }) {
     const allSelectedCards = Object.values(selectedCards).flat(1);
     const cards: Card[] = await this.broker.call('cards.get', { id: allSelectedCards });
@@ -204,6 +324,14 @@ export default class TurnHandler {
     return Object.fromEntries(entries);
   }
 
+  /**
+   * Returns a boolean determining whether everyone in the round has selected their cards.
+   *
+   * @protected
+   * @param {GameInterface} game
+   * @returns {boolean}
+   * @memberof TurnHandler
+   */
   protected hasEveryoneSelected(game: GameInterface): boolean {
     const { players, selectedCards, turnData } = game;
     // ensure every player, has a property in the selected cards map.
